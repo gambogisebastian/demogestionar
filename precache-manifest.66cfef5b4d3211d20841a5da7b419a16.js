b122bd9abbfa9f17b9ddbc6b5788c01f676862b4
gambogisebastian <sebastian.pablog@gmail.com>
2020-01-27 19:27:05 -0300

    Updates

--- precache-manifest.66cfef5b4d3211d20841a5da7b419a16.js
+++ precache-manifest.66cfef5b4d3211d20841a5da7b419a16.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a5a273ba8189d9633fda1cddd335fa25",
+    "revision": "b9510acdcf78b6e38fdfb2049fed6279",
     "url": "/demogestionar/index.html"
   },
   {
-    "revision": "57e51ac96e21ff0e0c6d",
-    "url": "/demogestionar/static/css/main.9aa96573.chunk.css"
+    "revision": "aaabd0bc900b4d1483de",
+    "url": "/demogestionar/static/css/main.67be2bd8.chunk.css"
   },
   {
-    "revision": "7cd344a6e8926d64b0d2",
-    "url": "/demogestionar/static/js/2.ffba50de.chunk.js"
+    "revision": "ff1f3b3ab7700ae0a5c1",
+    "url": "/demogestionar/static/js/2.b6c43897.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/demogestionar/static/js/2.ffba50de.chunk.js.LICENSE"
+    "url": "/demogestionar/static/js/2.b6c43897.chunk.js.LICENSE"
   },
   {
-    "revision": "57e51ac96e21ff0e0c6d",
-    "url": "/demogestionar/static/js/main.c9c8ea54.chunk.js"
+    "revision": "aaabd0bc900b4d1483de",
+    "url": "/demogestionar/static/js/main.8665ff48.chunk.js"
   },
   {
     "revision": "8c41883cb783a9cd88d9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demogestionar/precache-manifest.314f783c9f197802ae89f5f888588dd4.js"
+  "/demogestionar/precache-manifest.66cfef5b4d3211d20841a5da7b419a16.js"
 );
 
 self.addEventListener('message', (event) => {
